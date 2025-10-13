b30a11c9d65e1184539bdbb9d428533086f09ec8
202508083 <202508083@itc.ac.kr>
2025-10-13 11:37:59 +0900

    loops/main.go v0.1 if

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -3,11 +3,36 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
+	// shadowing
+	// var fmt string = "inha"
+	// var int int = 7
+	// var k int = 11
+	// fmt.Println(int)
+
 	r := bufio.NewReader(os.Stdin)
-	i, _ := r.ReadString('\n') // ignore error
-	fmt.Println(i)
+	i, err := r.ReadString('\n')
+	//fmt.Println(err)
+	if err != nil {
+		log.Fatal(err) // report the error and exit the program
+	}
+
+	i = strings.TrimSpace(i)
+	score, err := strconv.ParseFloat(i, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if score >= 60 {
+		fmt.Println("Pass")
+	} else {
+		fmt.Println("Fail")
+	}
+	fmt.Println(score)
 }
