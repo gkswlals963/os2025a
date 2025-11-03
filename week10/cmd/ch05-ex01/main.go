492206fab34aeeef83b8ee3dc14605c74711344d
202508083 <202508083@itc.ac.kr>
2025-11-03 12:05:02 +0900

    week10 array v0.5

--- week10/cmd/ch05-ex01/main.go
+++ week10/cmd/ch05-ex01/main.go
@@ -5,13 +5,13 @@ import (
 )
 
 func main() {
-	arrayBool := [3]bool{true, false, true}
+	arrayBool := [3]bool{true, false}
 	arrayInt := [3]int{-9, 11, 7}
 	for i := 0; i < 3; i++ {
 		fmt.Println(i, arrayInt[i])
-		fmt.Println(i, arrayBool[i])
+		//fmt.Println(i, arrayBool[i])  //runtime error
 	}
-	// fmt.Println(reflect.TypeOf(arrayBool))
-	// fmt.Printf("%#v\n", arrayBool)
-	// fmt.Printf("%#v\n", arrayInt)
+
+	fmt.Printf("%#v\n", arrayBool)
+	fmt.Printf("%#v\n", arrayInt)
 }
