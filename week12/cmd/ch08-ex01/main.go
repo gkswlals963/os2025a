b489ca9ab83242f1689244d1109201eef35a16f8
202508083 <202508083@itc.ac.kr>
2025-11-17 11:41:52 +0900

    week12 struct v0.2

--- week12/cmd/ch08-ex01/main.go
+++ week12/cmd/ch08-ex01/main.go
@@ -9,12 +9,16 @@ type subscriber struct {
 
 func applyPrice(s *subscriber) {
 	s.price = 10000
-	s.name = "Han jimin"
+	s.name = "Park Inha"
 }
 
 func main() {
 	var s1 subscriber
+	var p *subscriber = &s1
 	//s1.name = "Kim Inha"
 	applyPrice(&s1)
 	fmt.Println(s1.name, s1.price)
+	// fmt.Println(*p.price)
+	fmt.Println((*p).price)
+	fmt.Println(p.price)
 }
